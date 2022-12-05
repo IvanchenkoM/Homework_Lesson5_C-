09211d817b9e91b3f39b39577ca58a67c7c6b90e
Marina Ivanchenko <sunshine808@ya.ru>
2022-12-05 15:05:47 +0300

    Task36_3

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -4,11 +4,8 @@
 int number = ReadIn("Enter the number of array elements: ");
 int[] array= new int[number];
 
-for(int i=0; i<array.Length; i++)
-{
-    array[i]= new Random().Next(-10,11);
-    Console.Write(array[i]+" ");
-}
+FillArrayRandomNumbers(array);//функция заполнения массива
+PrintArray(array);//функция печати массива
 
 int sumOdd=0;
 for(int j=1;j<array.Length; j+=2)
@@ -22,4 +19,22 @@ int ReadIn(string msg)
 {
     Console.Write(msg);
     return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        array[i]= new Random().Next(-10,11);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    foreach (int i in array)
+    {
+        Console.Write(i + " ");
+    }
+    Console.Write("]");
 }
