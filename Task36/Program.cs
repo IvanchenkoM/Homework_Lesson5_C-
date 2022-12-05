f9ef76fb14f06fa2fdf39672b962b3ba52205e85
Marina Ivanchenko <sunshine808@ya.ru>
2022-12-05 14:38:28 +0300

    Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму эл-тов, стоящих в нечетных позициях.*/
+
+int number = ReadIn("Enter the number of array elements: ");
+int[] array= new int[number];
+
+for(int i=0; i<array.Length; i++)
+{
+    array[i]= new Random().Next(-10,11);
+    Console.Write(array[i]+" ");
+}
+
+int sumOdd=0;
+for(int j=1;j<array.Length; j+=2)
+{
+    sumOdd+=array[j];
+}
+
+Console.WriteLine($" -> The sum of odd array elements is {sumOdd}.");
+
+int ReadIn(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
