fc7f3d48db0355cd807ad7a7c0425cd844c5ca30
Marina Ivanchenko <sunshine808@ya.ru>
2022-12-05 14:25:39 +0300

    Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задайте массив заполненный случайными 3-хзначными числами. 
+Напишите программу, которая покажет кол-во четных чисел в массиве.*/
+
+int number = ReadIn("Enter the number of array elements: ");
+int[] array= new int[number];
+
+for(int i=0; i<array.Length; i++)
+{
+    array[i]= new Random().Next(100,1000);
+    Console.Write(array[i]+" ");
+}
+
+int countEven=0;
+for(int j=0;j<array.Length; j++)
+{
+    if(array[j]%2==0)
+    {
+        countEven++;
+    }
+}
+
+Console.WriteLine($" -> In this array are {countEven} even elements.");
+
+int ReadIn(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
